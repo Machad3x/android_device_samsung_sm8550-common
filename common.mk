ec819119b8e150854ffccc142841499090c112d4
Lup Gabriel <gwolf2u.website@gmail.com>
2022-02-24 01:20:27 +0530

    sm8550-common: Enable UDFPS icons and animations
    Signed-off-by: mukesh22584 <mks22584@gmail.com>

--- common.mk
+++ common.mk
@@ -153,6 +153,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
 
+# FOD
+TARGET_HAS_UDFPS := true
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
