a778577474bdcd760cecb66a1303eac59877facd
Machad3x <machad3x@gmail.com>
2025-02-08 00:25:50 -0400

    sm8550-common: Drop Samsung Doze

--- common.mk
+++ common.mk
@@ -91,10 +91,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
 
-# Doze
-PRODUCT_PACKAGES += \
-    SamsungDoze
-
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey
