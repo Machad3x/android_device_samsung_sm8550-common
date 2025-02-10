a3d07b16ba9c56583b83a03238f36ac580e18579
Machad3x <machad3x@gmail.com>
2025-02-09 21:54:52 -0400

    sm8550-common: init: Setup cpuset parameters

--- init/init.kernel.post_boot-kalama.sh
+++ init/init.kernel.post_boot-kalama.sh
@@ -113,7 +113,10 @@ echo $silver_early_upmigrate $gold_early_upmigrate > /proc/sys/walt/sched_early_
 echo 325 > /proc/sys/walt/walt_low_latency_task_threshold
 
 # cpuset parameters
-echo 0-2 > /dev/cpuset/background/cpus
+echo 1-2 > /dev/cpuset/audio-app/cpus
+echo 0-1 > /dev/cpuset/background/cpus
+echo 0-6 > /dev/cpuset/foreground/cpus
+echo 0-2 > /dev/cpuset/restricted/cpus
 echo 0-2 > /dev/cpuset/system-background/cpus
 
 # Turn off scheduler boost at the end
